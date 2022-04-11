b22ae1d50e574b7010fd41f4a4504e2cfc11da13
Hampus Rosenquist <hamro777@student.liu.se>
2022-04-11 20:16:12 +0200

    Add user modules

--- server/user/models.py
+++ server/user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+
+class Playlist(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    #songs = models.ManyToManyRel('music.Song')
+    is_public = models.BooleanField(default=False)
+    minutes = models.PositiveSmallIntegerField()
+    amount = models.PositiveSmallIntegerField()
+    date = models.DateField()
