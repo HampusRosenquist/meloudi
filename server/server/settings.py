4748e296698b09b50ac5e04302546b1127c30b32
Hampus Rosenquist <hamro777@student.liu.se>
2022-06-05 19:53:07 +0200

    Change to drf-jwt instead

--- server/server/settings.py
+++ server/server/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
+    'rest_framework_jwt',
     'corsheaders'
 ]
 
@@ -124,7 +124,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
